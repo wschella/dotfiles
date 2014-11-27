51bc643718b317a943e683c74c7807d31690acc2
Tom Naessens <Tom.Naessens@UGent.be>
2014-11-27 17:55:41 +0000

    Added bashrc histfile and setup.sh

---
+++ setup.sh
@@ -0,0 +1,37 @@
+# /usr/bin/bash
+
+REPO=http://github.com/SirDestrox/dotfiles
+DEST="$HOME/.dotfiles"
+
+if [[ -d $DEST ]];then
+  cd $DEST && git pull origin master && cd -
+else
+  echo "cloning repo: $REPO into dir: $DEST"
+  git clone $REPO $DEST
+fi
+
+echo "Setting up Vundle"
+if [[ -d $HOME/.vim/bundle/vundle ]]; then
+  echo "Vundle already installed"
+else
+  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+echo "Creating tmp dirs in .vim"
+mkdir -p $HOME/.vim/tmp/{backup,swap}
+
+echo "Linking dotfiles"
+dotfiles=(
+	zshrc
+	gitconfig
+	bashrc
+	vimrc
+)
+for dotfile in $dotfiles; do
+	ln -fn $DEST/$dotfile $HOME/.$dotfile
+done
+
+echo "updating Bundles"
+vim +BundleInstall! +qall
+
+reset
