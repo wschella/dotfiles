3d7fae80f3be2a99e326c7ee2b7aad67c0195fe4
Wout Schellaert <wout.schellaert@gmail.com>
2017-04-13 16:35:07 +0200

    Cleanup

--- setup.sh
+++ setup.sh
@@ -10,31 +10,12 @@ else
   git clone $REPO $DEST
 fi
 
-echo "Setting up Vundle"
-if [[ -d $HOME/.vim/bundle/vundle ]]; then
-  echo "Vundle already installed"
-else
-  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-fi
-
-echo "Creating tmp dirs in .vim"
-mkdir -p $HOME/.vim/tmp/{backup,swap}
-
 echo "Linking dotfiles"
 
 dotfiles=(
-	zshrc
-	bashrc
-	gitconfig
-  xinitrc
-  Xresources
-  vimrc
-  )
+)
 
 basicdotfiles=(
-  zshrc
-  bashrc
-  vimrc
 )
 
 if [ $1 == "-e" ];then
