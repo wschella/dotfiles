d43c632f1867b556c6c6fffe4403a9c74d84b094
Wout Schellaert <wout.schellaert@gmail.com>
2019-08-19 00:40:24 +0200

    Fix some aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,30 +1,34 @@
-alias python="python3"
-
+# Utilities
 alias fuck="sudo $(fc -ln -1)"
-
 alias please="sudo $(fc -ln -1)"
+alias l="ls -ahGl --color=auto"
 
 alias latexwatch="latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pvc -pdf"
 
-alias ls="ls -ahGl --color=auto"
+alias rebash="dotter -d ~/.dotfiles deploy && source ~/.bashrc"
 
-alias nanocl="bundle exec nanoc live"
-
-alias rebash="dotter -d ~/.dotfiles deploy && bash"
-
+# Version control
 alias gti="git"
 alias g="git"
 
-alias coqide='export OPAMROOT=~/opam-coq.8.8.1; eval $(opam config env); coqide&
-'
+# Networking
+alias ipa="ip -c a"
+alias ip4="ip -c -br -4 a"
+alias ip6="ip -c -br -6 a"
 
+# Copying and pasting yo
 alias c="xclip -selection clipboard"
 alias v="xclip -o -selection clipboard"
 
+# Password managing
 # https://lelutin.ca/posts/Using_password-store_with_an_alternate_directory/
 alias zpass='PASSWORD_STORE_DIR=~/.zeus-wachtwoord-winkel pass'
 
+# Programming environements
+alias python="python3"
 alias java8="/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar"
+alias nanocl="bundle exec nanoc live"
+alias coqide='export OPAMROOT=~/opam-coq.8.8.1; eval $(opam config env); coqide&'
 
 # Project CompBio
 alias smap="python3 ./meta/smap.py"
