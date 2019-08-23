96e236ad287606db4ca7b1d90966aa8e9489796c
Wout Schellaert <wout.schellaert@gmail.com>
2019-08-23 18:32:53 +0200

    Add NVM to bashrc to prevent it getting overriden on redeploy

--- bashrc.sh
+++ bashrc.sh
@@ -22,7 +22,13 @@ if [ -f $HOME/.bash_aliases ]; then
     . $HOME/.bash_aliases
 fi
 
+# Prompt
 eval "$(starship init bash)"
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
