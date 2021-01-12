38b9b9611463f8c3bb1506a63b06fee9681add24
Wout Schellaert <wout.schellaert@gmail.com>
2021-01-12 14:50:44 +0100

    Clean up some old unused aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -30,7 +30,3 @@ alias zpass='PASSWORD_STORE_DIR=~/.zeus-wachtwoord-winkel pass'
 # Programming environements
 alias java8="/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar"
 alias nanocl="bundle exec nanoc live"
-alias coqide='export OPAMROOT=~/opam-coq.8.8.1; eval $(opam config env); coqide&'
-
-# Project CompBio
-alias smap="python3 ./meta/smap.py"
