d3f9020504f92a53445b8eacff7419f323bc7e47
Wout Schellaert <wout.schellaert@gmail.com>
2021-02-26 13:47:08 +0100

    No more NVM

--- bashrc.sh
+++ bashrc.sh
@@ -25,11 +25,6 @@ fi
 # Prompt
 eval "$(starship init bash)"
 
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
 #------------------------------------------------------------------------------
