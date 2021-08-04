83d47a02de87beb141c359bd46158107f482ca4e
Wout Schellaert <wout.schellaert@gmail.com>
2021-08-04 11:07:56 +0200

    Add conda aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -23,6 +23,12 @@ alias ipa="ip -c a"
 alias ip4="ip -c -br -4 a"
 alias ip6="ip -c -br -6 a"
 
+# Conda
+alias condcre="conda env create --prefix .env -f environment.yml"
+alias condupt="conda env update --prefix ./env --file environment.yml  --prune"
+alias condact="conda activate ./.env"
+alias conddact="conda deactivate"
+
 # Copying and pasting yo
 alias c="xclip -selection clipboard"
 alias v="xclip -o -selection clipboard"
