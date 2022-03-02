9b6640c835695532d4b8fee6aaf90c56b7d2b1e9
Wout Schellaert <wout.schellaert@gmail.com>
2022-03-02 22:07:49 +0100

    Add jackt storage server

--- nix/machines/errink/nextcloud.nix
+++ nix/machines/errink/nextcloud.nix
@@ -43,14 +43,24 @@
     ];
   };
   
-  fileSystems."/mnt/storagebox" = {
+  fileSystems."/mnt/storagebox-brella" = {
       device = "//u275045.your-storagebox.de/backup";
       fsType = "cifs";
       options = let
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
         permission_opts = "uid=997,gid=998";
-      in ["${automount_opts},${permission_opts},credentials=/etc/nixos/smb-secrets"];
+      in ["${automount_opts},${permission_opts},credentials=/etc/nixos/smb-secrets-brella"];
+  };
+
+  fileSystems."/mnt/storagebox-jackt" = {
+      device = "//u293138.your-storagebox.de/backup";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        permission_opts = "uid=997,gid=998";
+      in ["${automount_opts},${permission_opts},credentials=/etc/nixos/smb-secrets-jackt"];
   };
 
   systemd.services."nextcloud-setup" = {
